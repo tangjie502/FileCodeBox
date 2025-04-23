98283e73849b26b38fb980fc8759be7b3db478a6
唐杰 <tangjie502@hotmail.com>
2025-04-23 16:53:47 +0800

    refactor: 删除未使用的图标组件
    - 移除了 AdminLayout-DDCPshEE.js 中未使用的图标组件
    - 删除了 box-DBrNLmg8.js、clipboard-BsVvyWeO.js 中的所有内容
    - 这些文件和图标组件在当前代码中没有被使用，删除以简化代码结构

--- themes/2024/assets/FileManageView-CyCt60hk.js
+++ themes/2024/assets/FileManageView-CyCt60hk.js
@@ -1,4 +1,4 @@
-import{c as w,d as R,r as M,u as Y,f as L,a as u,b as t,l as G,n as i,h as e,i as J,m as x,g as l,v as y,L as N,s as h,F as S,q as $,t as g,A as j,e as p}from"./index-CgqZQa96.js";import{F as O}from"./file-oKDOd1HK.js";import{T as Q}from"./trash-cjXYkUOS.js";/**
+import{c as w,d as R,r as M,u as Y,f as L,a as u,b as t,l as G,n as i,h as e,i as J,m as x,g as l,v as y,L as N,s as h,F as S,q as $,t as g,A as j,e as p}from"./index-j4oDeyJj.js";import{F as O}from"./file-Bk0HNQ5t.js";import{T as Q}from"./trash-CB_DXwOq.js";/**
  * @license lucide-vue-next v0.445.0 - ISC
  *
  * This source code is licensed under the ISC license.
